bed7e88c63805091eb07899c0b4ed30bb0b6adc2
ruia1 <past1215@gmail.com>
2018-10-13 13:12:02 +0900

    qkのtkinterを用いたGUIの作製(完了)

--- qk.py
+++ qk.py
@@ -1,38 +1,38 @@
 # -*- coding: utf-8 -*-
 #各モードを開くためのプログラム
-
+from tkinter import *
 import goropri,primalytest,gamemode,psearch,panalyze
-
-def qkhelp():
-    #"試合モード:試合進行を、素数判定や記録でサポートします。\n"+
-    print("素数判定　:素数判定をします。\n"+
-          "素数探索　:素数を様々な条件で探索できます。\n"+
-          "語呂モード:語呂素数の探索をします。\n")
-          #"分析モード:試合モードで記録したデータや、CPU対戦")#分析モード準備中
-    
+def f1():
+    qkh1.destroy()
+    primalytest.pthome()
+def f2():
+    qkh1.destroy()
+    psearch.sehome()
+def f3():
+    qkh1.destroy()
+    goropri.gohome()
+modes = [["素数判定モード","素数判定をします。",f1],
+         ["素数探索モード","素数を様々な条件で探索できます。",f2],
+         ["語呂モード","語呂素数の探索をします。",f3]]
+class qkhc(Frame):
+    def __init__(self,master=None):
+        super().__init__(master)
+        self.pack()
+        self.createw()
+    def createw(self):
+        self.qkhlb = Label(self,text="進みたいモードのボタンを押してください。")
+        self.qkhlb.grid(row=0,column=0,columnspan=2,padx=5,pady=5)
+        for i in range(3):
+            self.qkhptestb = Button(self,text=modes[i][0],command = modes[i][2])
+            self.qkhptestb.grid(row=i+1,column=0,padx=5,pady=5,sticky=W+E)
+            self.qkhptestl = Label(self,text=modes[i][1])
+            self.qkhptestl.grid(row=i+1,column=1,padx=5,pady=5,sticky=W)
+        self.qkhexit = Button(self,text="終了",command = qkh1.destroy)
+        self.qkhexit.grid(row=4,column=0,columnspan=2,padx=5,pady=5,sticky=W+E)
+        
 def qkhome():
-    while True:
-        print("QK Supporter ホームです。すすみたいモードのコードを入力してください。\n"+
-              "素数判定:ptest 素数探索:search 語呂モード:goro\n"+
-              "ヘルプ:help 終了:end")
-              #"分析モード:analyze ヘルプ:help 終了:end")
-        modes = input()
-        print()
-        if modes == "end":
-            print("終了します。")
-            input("終了するにはエンターを押してください...")
-            return
-        elif modes == "ptest":#素数判定モード
-            primalytest.pthome()
-        #elif modes == "game":#試合モード
-        #    gamemode.gmhome()
-        elif modes == "search":#素数探索
-            psearch.sehome()
-        elif modes == "goro":#語呂モード
-            goropri.gohome()
-        #elif modes == "analyze":#分析モード
-        #    panalyze.anhome()
-        elif modes == "help":
-            qkhelp()
-        else:
-            print("正しい値を入力してください。")
+    qkh1 = Tk()
+    qkh1.title="QK Supporter ホーム"
+    qkh2 = qkhc(qkh1)
+    qkh2.mainloop()
+    
